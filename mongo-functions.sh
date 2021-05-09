61fe623d9ac0e3891f35a2a53f6eee50ae3afa94
Antonio Fuschetto <antonio.fuschetto@mongodb.com>
2021-05-09 00:13:41 +0000

    Add documentation to mongo functions

--- mongo-functions.sh
+++ mongo-functions.sh
@@ -2,18 +2,17 @@
 ### Repository management
 ###
 
-__mongo-check-workdir ()
-{
-    if [[ ! -d buildscripts ]]; then
-        echo "ERROR: ${PWD} is not a mongo working directory" 1>&2;
-        exit 1;
-    fi
-}
-
+# Reset the working directory to its initial state, as if the repository had
+# just been cloned and its Python virtual environment properly created and
+# initialized.
+#
+# Quality level: Successfully tested with branches 4.4 and higher.
+# TODO: * Add '--use-feature=2020-resolver' argument to the 'pip install'
+#         command as required for branches 4.0 and 4.2.
 mongo-reset ()
 {
     ( set -e;
-    __mongo-check-workdir;
+    __mongo-check-wrkdir;
 
     [[ -n ${VIRTUAL_ENV} ]] && deactivate;
     git clean -fdx;
@@ -27,35 +26,64 @@ mongo-reset ()
 ### Build procedures
 ###
 
-# TODO: Add a parameter for --ninja (default) and --no-ninja
+# Generate the configuration file required by the Ninja build system. Such
+# configuration is automatically generated or updated when "mongo-build" is run,
+# however this function must be explicitly executed when a SCons configuration
+# file changes (e.g., after adding or removing a source file to the project).
+#
+# Quality level: Successfully tested with branches 4.4 and higher.
+# TODO: * Inhibit the execution for branches 4.0 and 4.2.
 mongo-configure ()
 {
     ( set -e;
-    __check-mongo-dir;
+    __mongo-check-wrkdir;
 
     ./buildscripts/scons.py \
         --variables-files=etc/scons/mongodbtoolchain_stable_clang.vars \
         --opt=off \
         --dbg=on \
+	--link-model=dynamic \
         --ninja generate-ninja \
         ICECC=icecc \
         CCACHE=ccache )
 }
 
-# TODO: Add a parameter for --ninja (default) and --no-ninja
+# Build the mongo project taking care of the environment configuration
+# (mongo-configure) when required.
+#
+# Options:
+#   --ninja:    Leverage the Ninja build system to spread and orchestrate the
+#               build across a large set of hosts. The feature is fully
+#               supported starting with version 4.4 of mongo. This option is
+#               enabled by default.
+#   --no-ninja: Do not take advantage of the Ninja build system and the build
+#               runs only on the local host. This option is mandatory to build
+#               version 4.2 of mongo.
+#
+# Quality level: Successfully tested with branches 4.2 and higher.
+# TODO: * Suppor branch 4.0
 mongo-build ()
 {
     ( set -e;
-    __mongo-check-workdir;
+    __mongo-check-wrkdir;
 
-    [[ -f build.ninja ]] || mongo-configure;
-    ninja -j400 install-all )
+    __mongo-parse-args $@;
+    if ${__use_ninja}; then
+        [[ -f build.ninja ]] || mongo-configure;
+        ninja -j400 install-all;
+    else
+        ./buildscripts/scons.py \
+	    --variables-files=etc/scons/mongodbtoolchain_stable_clang.vars \
+	    --opt=off \
+	    --dbg=on \
+	    ICECC=icecc;
+    fi )
 }
 
 mongo-clean ()
 {
     ( set -e;
-    __mongo-check-workdir;
+    __mongo-check-wrkdir;
 
     ninja -t clean;
     ccache -c )
@@ -64,7 +92,7 @@ mongo-clean ()
 mongo-index ()
 {
     ( set -e;
-    __mongo-check-workdir;
+    __mongo-check-wrkdir;
 
     ./buildscripts/scons.py \
         --variables-files=etc/scons/mongodbtoolchain_stable_clang.vars \
@@ -78,7 +106,7 @@ mongo-index ()
 mongo-format ()
 {
     ( set -e;
-    __mongo-check-workdir;
+    __mongo-check-wrkdir;
 
     ./buildscripts/clang_format.py format-my )
 }
@@ -89,7 +117,7 @@ mongo-format ()
 
 # TODO: Require the JS file as mandatory argument
 function mongo-test {( set -e
-    $(__mongo-check-workdir)
+    $(__mongo-check-wrkdir)
 
     ./buildscripts/resmoke.py run \
         --storageEngine=wiredTiger \
@@ -106,7 +134,7 @@ function mongo-test {( set -e
 
 # TODO: Find a way to automatically set the patch summary including the patch set number
 function mongo-send-evergreenpatch {( set -e
-    $(__mongo-check-workdir)
+    $(__mongo-check-wrkdir)
 
     evergreen patch \
         --project mongodb-mongo-master \
@@ -119,7 +147,7 @@ function mongo-send-evergreenpatch {( set -e
 
 # TODO: Fix stacktrace
 function mongo-send-codereview {( set -e
-    $(__mongo-check-workdir)
+    $(__mongo-check-wrkdir)
 
     .venv/bin/python3 ${HOME}/support/kernel-tools/codereview/upload.py \
         --git_similarity=100 \
@@ -130,3 +158,36 @@ function mongo-send-codereview {( set -e
         --jira_user "antonio.fuschetto" \
         $@
 )}
+
+###
+### Utilities
+###
+
+__mongo-check-wrkdir ()
+{
+    if [[ ! -d buildscripts ]]; then
+        echo "ERROR: ${PWD} is not a mongo working directory" 1>&2;
+        exit 1;
+    fi
+}
+
+__mongo-parse-args ()
+{
+    __use_ninja=true;
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            --ninja)
+                __use_ninja=true;
+                shift
+            ;;
+            --no-ninja)
+                __use_ninja=false;
+                shift
+            ;;
+            *)
+                echo "ERROR: $1 is not a supported parameter" 1>&2;
+                exit 1
+            ;;
+        esac;
+    done
+}
