e60f64e3836b156b614c3e421eb3b1b304a774dc
Antonio Fuschetto <antonio.fuschetto@mongodb.com>
2022-04-08 22:32:07 +0200

    Format Mongo functions for Linux

--- mongodb/mongo-functions.sh
+++ mongodb/mongo-functions.sh
@@ -22,19 +22,19 @@ mongo-prepare ()
 	${__cmd_prefix} ccache -C;
 
 	case ${__mongo_branch} in
-	v4.2 | v4.4 | v5.0 | v5.3 | master)
-		${__cmd_prefix} \python3 -m venv .venv;
-		${__cmd_prefix} . .venv/bin/activate;
-		${__cmd_prefix} .venv/bin/python3 -m pip install -r buildscripts/requirements.txt --use-feature=2020-resolver
+		v4.2 | v4.4 | v5.0 | v5.3 | master)
+			${__cmd_prefix} \python3 -m venv .venv;
+			${__cmd_prefix} . .venv/bin/activate;
+			${__cmd_prefix} .venv/bin/python3 -m pip install -r buildscripts/requirements.txt --use-feature=2020-resolver
 		;;
-	v4.0)
-		${__cmd_prefix} \virtualenv -p python2 .venv;
-		${__cmd_prefix} . .venv/bin/activate;
-		${__cmd_prefix} .venv/bin/python2 -m pip install -r buildscripts/requirements.txt --use-feature=2020-resolver
+		v4.0)
+			${__cmd_prefix} \virtualenv -p python2 .venv;
+			${__cmd_prefix} . .venv/bin/activate;
+			${__cmd_prefix} .venv/bin/python2 -m pip install -r buildscripts/requirements.txt --use-feature=2020-resolver
 		;;
-	*)
-		echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-		return 1
+		*)
+			echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+			return 1
 		;;
 	esac )
 }
@@ -81,31 +81,31 @@ mongo-build ()
 	__mongo-check-wrkdir;
 	__mongo-parse-args $@;
 
-	[[ ${__format} == 1 ]] && ${__cmd_prefix} mongo-format ${__mongo_branch}
+	[[ ${__format} == 1 ]] && ${__cmd_prefix} mongo-format ${__mongo_branch};
 
 	case ${__mongo_branch} in
-	v4.4 | v5.0 | v5.3 | master)
-		[[ -f build.ninja ]] || __mongo-configure-ninja $@;
-		[[ -f compile_commands.json ]] || __mongo-configure-compilation-db $@;
-		${__cmd_prefix} ninja \
-			-j400 \
-			generated-sources \
-			install-${__target} \
-			${__args[@]}
+		v4.4 | v5.0 | v5.3 | master)
+			[[ -f build.ninja ]] || __mongo-configure-ninja $@;
+			[[ -f compile_commands.json ]] || __mongo-configure-compilation-db $@;
+			${__cmd_prefix} ninja \
+					-j400 \
+					generated-sources \
+					install-${__target} \
+					${__args[@]}
 		;;
-	v4.0 | v4.2)
-		[[ -f compile_commands.json ]] || __mongo-configure-compilation-db $@;
-		${__cmd_prefix} ./buildscripts/scons.py \
-			--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
-			${__build_mode} \
-			ICECC=icecc \
-			generated-sources \
-			${__target} \
-			${__args[@]}
+		v4.0 | v4.2)
+			[[ -f compile_commands.json ]] || __mongo-configure-compilation-db $@;
+			${__cmd_prefix} ./buildscripts/scons.py \
+					--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
+					${__build_mode} \
+					ICECC=icecc \
+					generated-sources \
+					${__target} \
+					${__args[@]}
 		;;
-	*)
-		echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-		return 1
+		*)
+			echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+			return 1
 		;;
 	esac )
 }
@@ -127,19 +127,19 @@ mongo-clean ()
 	__mongo-parse-args $@;
 
 	case ${__mongo_branch} in
-	v4.4 | v5.0 | v5.3 | master)
-		${__cmd_prefix} ninja -t clean;
-		${__cmd_prefix} ccache -c
+		v4.4 | v5.0 | v5.3 | master)
+			${__cmd_prefix} ninja -t clean;
+			${__cmd_prefix} ccache -c
 		;;
-	v4.0 | v4.2)
-		${__cmd_prefix} ./buildscripts/scons.py \
-			--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
-			--clean \
-			${__target}
+		v4.0 | v4.2)
+			${__cmd_prefix} ./buildscripts/scons.py \
+					--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
+					--clean \
+					${__target}
 		;;
-	*)
-		echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-		return 1
+		*)
+			echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+			return 1
 		;;
 	esac )
 }
@@ -157,15 +157,15 @@ mongo-format ()
 	__mongo-parse-args $@;
 
 	case ${__mongo_branch} in
-	v4.4 | v5.0 | v5.3 | master)
-		${__cmd_prefix} ./buildscripts/clang_format.py format-my
+		v4.4 | v5.0 | v5.3 | master)
+			${__cmd_prefix} ./buildscripts/clang_format.py format-my
 		;;
-	v4.0 | v4.2)
-		${__cmd_prefix} ./buildscripts/clang_format.py format
+		v4.0 | v4.2)
+			${__cmd_prefix} ./buildscripts/clang_format.py format
 		;;
-	*)
-		echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-		return 1
+		*)
+			echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+			return 1
 		;;
 	esac )
 }
@@ -186,13 +186,12 @@ mongo-test-locally ()
 	${__cmd_prefix} \rm -f executor.log fixture.log tests.log;
 	set +e;
 	${__cmd_prefix} ./buildscripts/resmoke.py run \
-		--storageEngine=wiredTiger \
-		--storageEngineCacheSizeGB=0.5 \
-		--mongodSetParameters='{logComponentVerbosity: {verbosity: 2}}' \
-		--jobs=${__tasks} \
-		--log=file \
-		${__args[@]} )
-	#[[ $? == 0 ]] && echo -e '>> \e[0;32mPASSED\e[0m <<' || echo -e '>> \e[0;31mFAILED\e[0m <<' )
+			--storageEngine=wiredTiger \
+			--storageEngineCacheSizeGB=0.5 \
+			--mongodSetParameters='{logComponentVerbosity: {verbosity: 2}}' \
+			--jobs=${__tasks} \
+			--log=file \
+			${__args[@]} )
 }
 
 # Creates a new Evergreen path where it is possible to select the specific
@@ -206,39 +205,23 @@ mongo-test-remotely ()
 	__mongo-check-wrkdir;
 	__mongo-parse-args $@;
 
-	msg=$(git log -n 1 --pretty=%B | head -n 1)
+	msg=$(git log -n 1 --pretty=%B | head -n 1);
 	if [[ ${__cmd_prefix} != echo ]]; then
-		echo ${msg}
-		read -p "Do you want to use this title for the Evergreen patch? [Y/n] ";
-		if [[ ${REPLY} =~ (n|N) ]]; then
-			read -p "Type the custom title: " msg
-		fi
-	fi
+		echo ${msg};
+		read -p "Do you want change the title of this Evergreen patch? [y/N] ";
+		if [[ ${REPLY} =~ (y|Y) ]]; then
+			read -p "Type the custom title: " msg;
+		fi;
+	fi;
 
 	${__cmd_prefix} evergreen patch \
-		--project mongodb-mongo-${__mongo_branch} \
-		--skip_confirm \
-		--description "$(git branch --show-current) ${msg}" \
-		${__args[@]} )
+			--project mongodb-mongo-${__mongo_branch} \
+			--skip_confirm \
+			--description "$(git branch --show-current) ${msg}" \
+			${__args[@]} )
 }
 
 ################################################################################
-
-mongo-verify-tee ()
-{
-	( set -e;
-	__mongo-check-wrkdir;
-	__mongo-parse-args --master $@;
-
-	${__cmd_prefix} \rm -f executor.log fixture.log tests.log;
-	${__cmd_prefix} ./buildscripts/resmoke.py run \
-		--storageEngine=wiredTiger \
-		--storageEngineCacheSizeGB=0.5 \
-		--mongodSetParameters='{logComponentVerbosity: {verbosity: 2}}' \
-		--jobs=${__tasks} \
-		${__args[@]} \
-		| tee tests.log )
-}
 
 mongo-merge ()
 {
@@ -247,8 +230,8 @@ mongo-merge ()
 	__mongo-parse-args $@;
 
 	${__cmd_prefix} evergreen commit-queue merge \
-		--project mongodb-mongo-${__mongo_branch} \
-		${__args[@]} )
+			--project mongodb-mongo-${__mongo_branch} \
+			${__args[@]} )
 }
 
 mongo-debug ()
@@ -256,14 +239,14 @@ mongo-debug ()
 	( set -e;
 	__mongo-parse-args $@;
 
-	echo __cmd_prefix=${__cmd_prefix}
-	echo __mongo_branch=${__mongo_branch}
-	echo __toolchain=${__toolchain}
-	echo __build_mode=${__build_mode}
-	echo __link_model=${__link_model}
-	echo __format=${__format}
-	echo __target=${__target}
-	echo __tasks=${__tasks}
+	echo __cmd_prefix=${__cmd_prefix};
+	echo __mongo_branch=${__mongo_branch};
+	echo __toolchain=${__toolchain};
+	echo __build_mode=${__build_mode};
+	echo __link_model=${__link_model};
+	echo __format=${__format};
+	echo __target=${__target};
+	echo __tasks=${__tasks};
 	echo __args=${__args} )
 }
 
@@ -285,110 +268,106 @@ __mongo-parse-args ()
 {
 	[[ -z ${__parsed_args} ]] && __parsed_args=true || return 0;
 
-	__cmd_prefix=
-	__mongo_branch=master
-	__toolchain=clang
-	__build_mode='--opt=off --dbg=on'
-	__link_model='--link-model=dynamic'
-	__format=1
-	__target=all
-	__tasks=1
-	__args=()
+	__cmd_prefix=;
+	__mongo_branch=master;
+	__toolchain=clang;
+	__build_mode='--opt=off --dbg=on';
+	__link_model='--link-model=dynamic';
+	__format=1;
+	__target=all;
+	__tasks=1;
+	__args=();
 
 	while [[ $# -gt 0 ]]; do
-	case $1 in
-		--echo)
-			__cmd_prefix=echo;
-			shift
+		case $1 in
+			--echo)
+				__cmd_prefix=echo;
+				shift
 			;;
-		--master)
-			__mongo_branch=master;
-			shift
+			--master)
+				__mongo_branch=master;
+				shift
 			;;
-		--v5.3)
-			__mongo_branch=v5.3;
-			shift
+			--v5.3)
+				__mongo_branch=v5.3;
+				shift
 			;;
-		--v5.0)
-			__mongo_branch=v5.0;
-			shift
+			--v5.0)
+				__mongo_branch=v5.0;
+				shift
 			;;
-		--v4.4)
-			__mongo_branch=v4.4;
-			shift
+			--v4.4)
+				__mongo_branch=v4.4;
+				shift
 			;;
-		--v4.2)
-			__mongo_branch=v4.2;
-			shift
+			--v4.2)
+				__mongo_branch=v4.2;
+				shift
 			;;
-		--v4.0)
-			__mongo_branch=v4.0;
-			shift
+			--v4.0)
+				__mongo_branch=v4.0;
+				shift
 			;;
-		--clang)
-			__toolchain=clang;
-			shift
+			--clang)
+				__toolchain=clang;
+				shift
 			;;
-		--gcc)
-			__toolchain=gcc;
-			shift
+			--gcc)
+				__toolchain=gcc;
+				shift
 			;;
-		--debug)
-			__build_mode='--opt=off --dbg=on'
-			shift
+			--debug)
+				__build_mode='--opt=off --dbg=on';
+				shift
 			;;
-		--release)
-			__build_mode='--opt=on --dbg=off'
-			shift
+			--release)
+				__build_mode='--opt=on --dbg=off';
+				shift
 			;;
-		--dynamic)
-			__link_model='--link-model=dynamic'
-			shift
+			--dynamic)
+				__link_model='--link-model=dynamic';
+				shift
 			;;
-		--static)
-			__link_model='--link-model=static'
-			shift
+			--static)
+				__link_model='--link-model=static';
+				shift
 			;;
-		--format)
-			__format=1
-			shift
+			--format)
+				__format=1;
+				shift
 			;;
-		--no-format)
-			__format=0
-			shift
+			--no-format)
+				__format=0;
+				shift
 			;;
-		--all)
-			__target=all;
-			shift
+			--all)
+				__target=all;
+				shift
 			;;
-		--core)
-			__target=core;
-			shift
+			--core)
+				__target=core;
+				shift
 			;;
-		--mono-task)
-			__tasks=1
-			shift
+			--mono-task)
+				__tasks=1;
+				shift
 			;;
-		--multi-task)
-			__tasks=`cat /proc/cpuinfo | grep processor | wc -l`
-			shift
+			--multi-task)
+				__tasks=`cat /proc/cpuinfo | grep processor | wc -l`;
+				shift
 			;;
-		*)
-			#if [[ $1 == -* ]]; then
-			#	echo "ERROR: $1 is not a supported option" 1>&2;
-			#	return 1;
-			#fi
-			__args+=($1)
-			shift
+			*)
+				__args+=($1);
+				shift
 			;;
-	esac;
+		esac;
 	done;
 
-	#if [[ ${__mongo_branch} != master && ${__mongo_branch} != v5.0 && ${__mongo_branch} != v4.4 && ${__mongo_branch} != v4.2 && ${__mongo_branch} != v4.0 ]]; then
-	#	echo "WARNING: ${__mongo_branch} is not a Git origin branch" 1>&2;
-	#	read -p "Do you want to use the master branch as a reference? [y/N] ";
-	#	[[ ${REPLY} =~ (y|Y) ]] && __mongo_branch=master || return 2;
-	#fi
+	# if [[ ${__mongo_branch} != master && ${__mongo_branch} != v5.0 && ${__mongo_branch} != v4.4 && ${__mongo_branch} != v4.2 && ${__mongo_branch} != v4.0 ]]; then
+	# 	echo "WARNING: ${__mongo_branch} is not a Git origin branch" 1>&2;
+	# 	read -p "Do you want to use the master branch as a reference? [y/N] ";
+	# 	[[ ${REPLY} =~ (y|Y) ]] && __mongo_branch=master || return 2;
+	# fi
 }
 
 __mongo-configure-ninja ()
@@ -398,30 +377,30 @@ __mongo-configure-ninja ()
 	__mongo-parse-args $@;
 
 	case ${__mongo_branch} in
-	master)
-		${__cmd_prefix} ./buildscripts/scons.py \
-			--variables-files=etc/scons/mongodbtoolchain_v4_${__toolchain}.vars \
-			${__build_mode} \
-			${__link_model} \
-			--ninja generate-ninja \
-			ICECC=icecc \
-			CCACHE=ccache \
-			${__args[@]}
+		master)
+			${__cmd_prefix} ./buildscripts/scons.py \
+					--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
+					${__build_mode} \
+					${__link_model} \
+					--ninja generate-ninja \
+					ICECC=icecc \
+					CCACHE=ccache \
+					${__args[@]}
 		;;
-	v4.4 | v5.0 | v5.3)
-		${__cmd_prefix} ./buildscripts/scons.py \
-			--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
-			${__build_mode} \
-			${__link_model} \
-			--ninja generate-ninja \
-			ICECC=icecc \
-			CCACHE=ccache \
-			${__args[@]}
+		v4.4 | v5.0 | v5.3)
+			${__cmd_prefix} ./buildscripts/scons.py \
+					--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
+					${__build_mode} \
+					${__link_model} \
+					--ninja generate-ninja \
+					ICECC=icecc \
+					CCACHE=ccache \
+					${__args[@]}
 		;;
-	#*)
-	#	echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-	#	return 1
-	#	;;
+		# *)
+		# 	echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+		# 	return 1
+		# ;;
 	esac )
 }
 
@@ -432,22 +411,22 @@ __mongo-configure-compilation-db ()
 	__mongo-parse-args $@;
 
 	case ${__mongo_branch} in
-	v4.4 | v5.0 | v5.3 | master)
-		${__cmd_prefix} ninja \
-			compiledb \
-			${__args[@]}
+		v4.4 | v5.0 | v5.3 | master)
+			${__cmd_prefix} ninja \
+					compiledb \
+					${__args[@]}
 		;;
-	v4.0 | v4.2)
-		${__cmd_prefix} ./buildscripts/scons.py \
-			--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
-			${__build_mode} \
-			ICECC=icecc \
-			compiledb \
-			${__args[@]}
+		v4.0 | v4.2)
+			${__cmd_prefix} ./buildscripts/scons.py \
+					--variables-files=etc/scons/mongodbtoolchain_stable_${__toolchain}.vars \
+					${__build_mode} \
+					ICECC=icecc \
+					compiledb \
+					${__args[@]}
 		;;
-	*)
-		echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
-		return 1
+		*)
+			echo "ERROR: ${__mongo_branch} branch is not supported by ${FUNCNAME[0]}" 1>&2;
+			return 1
 		;;
 	esac )
 }
